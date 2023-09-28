6e0969c7aa5f9dc981d2190b1169391c9a11438e
chimekinglsey <onlinekingsley@gmail.com>
2023-09-28 22:58:49 +0100

    A class for holberton course

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,19 +1,14 @@
+/*
+* this file contains a class for creating HolbertonCourse
+*/
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    // ==========================================================================================
-    // the commented block worked but last check is failing, lets try it per setter method
-    // ==========================================================================================
-    // if (
-    //   typeof name !== 'string'
-    //         || typeof length !== 'number'
-    //         || !Array.isArray(students)
-    //         || (students.every((item) => typeof item === 'string') !== true)
-    // ) throw new Error('Usage: name(string), length(number), students(an array)');
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
+  // getter method for name
   get name() {
     return this._name;
   }
@@ -21,11 +16,12 @@ export default class HolbertonCourse {
   /**
      * @param {string} name
      */
-  set name(Name) {
-    if (typeof Name !== 'string') throw new TypeError(`${Name} is not a string`);
-    this._name = Name;
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
   }
 
+  // getter method for lenght
   get length() {
     return this._length;
   }
@@ -33,22 +29,32 @@ export default class HolbertonCourse {
   /**
      * @param {number} length
      */
-  set length(Lenght) {
-    if (typeof Lenght !== 'number') throw new TypeError(`${Lenght} is not a valid number`);
-    this._length = Lenght;
+  set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    this._length = value;
   }
 
+  // getter for students array
   get students() {
     return this._students;
   }
 
   /**
-     * @param {string[]} students
+   * @param {string[]} students
      */
-  set students(Students) {
-    if (!Array.isArray(Students)) throw new TypeError(`${Students} must be an array`);
-    else if (Students.every((student) => typeof student !== 'string')) throw new TypeError('All  entries in the array must be a string type');
-
-    this._students = Students;
+  set students(value) {
+    if (!Array.isArray(value)) throw new TypeError('students must be an array');
+    if (value.every((student) => typeof student !== 'string')) throw new TypeError('All  entries in the array must be a string');
+    else this._students = value;
   }
 }
+
+// ==========================================================================================
+// the commented block worked but last check is failing, lets try it per setter method
+// ==========================================================================================
+// if (
+//   typeof name !== 'string'
+//         || typeof length !== 'number'
+//         || !Array.isArray(students)
+//         || (students.every((item) => typeof item === 'string') !== true)
+// ) throw new Error('Usage: name(string), length(number), students(an array)');
