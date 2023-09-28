305f8770de698a087e4bec50728d11fa97739714
chimekinglsey <onlinekingsley@gmail.com>
2023-09-28 04:48:29 +0100

    multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,27 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firsName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firsName, lastName), uploadPhoto(fileName)])
-    .then((result) => {
-      for (const func of result) {
-        if (func.value !== 'fulfilled') {
-          func.value = func.reason
-            [
-              {
-                status: func.status,
-                value: func.value,
-              }
-            ];
-        }
-      }
-    });
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
+
+// export default function handleProfileSignup(firsName, lastName, fileName) {
+//   return Promise.allSettled([signUpUser(firsName, lastName), uploadPhoto(fileName)])
+//     .then((result) => {
+//       for (const func of result) {
+//         if (func.value !== 'fulfilled') {
+//           func.value = func.reason
+//             [
+//               {
+//                 status: func.status,
+//                 value: func.value,
+//               }
+//             ];
+//         }
+//       }
+//     });
+// }
