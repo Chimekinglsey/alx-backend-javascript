568cfd7e337415b109c0e8857e70a0dbfa71cca6
chimekinglsey <onlinekingsley@gmail.com>
2023-09-28 03:02:08 +0100

    switched from async to promises .then()

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,16 @@
 // append handlers
 export default function handleResponseFromAPI(promise) {
-    const obj = {
-      status: 200,
-      body: 'Success',
-    };
-    return promise
+  const obj = {
+    status: 200,
+    body: 'Success',
+  };
+  return promise
     .then(() => obj)
-    .catch((error)=> new Error())
-    .finally(()=>console.log('Got a response from the API'))
-// commented out async to test this
-//   try {
-//     const result = await promise;
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+  // commented out async to test this
+  //   try {
+  //     const result = await promise;
 
 //     console.log(result)
 //     return obj;
