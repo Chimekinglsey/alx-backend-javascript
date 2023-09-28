54fc66ca9cb71d230fadd45044c921d901cf9a70
chimekinglsey <onlinekingsley@gmail.com>
2023-09-28 22:35:25 +0100

    Holberton Course class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,14 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (
-      typeof name !== 'string'
-            || typeof length !== 'number'
-            || !Array.isArray(students)
-            || (students.every((item) => typeof item === 'string') !== true)
-    ) throw new Error('Usage: name(string), length(number), students(an array)');
+    // ==========================================================================================
+    // the commented block worked but last check is failing, lets try it per setter method
+    // ==========================================================================================
+    // if (
+    //   typeof name !== 'string'
+    //         || typeof length !== 'number'
+    //         || !Array.isArray(students)
+    //         || (students.every((item) => typeof item === 'string') !== true)
+    // ) throw new Error('Usage: name(string), length(number), students(an array)');
     this._name = name;
     this._length = length;
     this._students = students;
@@ -19,6 +22,7 @@ export default class HolbertonCourse {
      * @param {string} name
      */
   set name(name) {
+    if (typeof name !== 'string') throw new TypeError(`${name} is not a string`);
     this._name = name;
   }
 
@@ -30,6 +34,7 @@ export default class HolbertonCourse {
      * @param {number} length
      */
   set length(length) {
+    if (typeof length !== 'number') throw new TypeError(`${length} is not a valid number`);
     this._length = length;
   }
 
@@ -41,6 +46,9 @@ export default class HolbertonCourse {
      * @param {string[]} students
      */
   set students(students) {
+    if (!Array.isArray(students)) throw new TypeError(`${students} must be an array`);
+    else if (students.every((student) => typeof student !== 'string')) throw new TypeError('All  entries in the array must be a string type');
+
     this._students = students;
   }
 }
