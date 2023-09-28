083c87f94a60200d37e7a7343bf0331aec1e809b
chimekinglsey <onlinekingsley@gmail.com>
2023-09-28 01:06:16 +0100

    a function that returns a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -4,16 +4,7 @@ function getResponseFromAPI() {
     resolve(true);
   });
 }
-// testing the funtion so jest can let me go
-describe('getResponseFromAPI', () => {
-  it('should resolve with a URL', async () => {
-    expect.assertions(1);
-    // No need for 'return' here
-    const response = await getResponseFromAPI();
-    expect(response).toBe(true);
-  });
-});
 
-// getResponseFromAPI()
+getResponseFromAPI();
 //   .then((data) => console.log(data))
 //   .catch((error) => console.log(error));
