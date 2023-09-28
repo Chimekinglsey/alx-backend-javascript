89c5dceb78328dc975aa6cdabe8b7c7021283b13
chimekinglsey <onlinekingsley@gmail.com>
2023-09-28 03:39:46 +0100

    using Promise.all() to handle multiple successful messages

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+// retrieves functions what return promises and display their data
+import { uploadPhoto, createUser } from './utils';
+
+const { firstName, lastName } = createUser;
+const { body } = uploadPhoto;
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto, createUser])
+    .then(() => console.log(`${body} ${firstName} ${lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
