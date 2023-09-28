60aa1242c9bc1af2fdb648d13380af8c99a9b13d
chimekinglsey <onlinekingsley@gmail.com>
2023-09-28 05:52:45 +0100

    calculator

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,8 @@
 export default function divideFunction(numerator, denominator) {
   if (isNaN(numerator) || isNaN(denominator)) {
-    throw new Error('Enter only numbers');
+    throw Error('Enter only numbers');
   } else if (denominator === 0) {
-    throw new Error('Cannot divide by 0');
+    throw Error('Cannot divide by 0');
   } else {
     return numerator / denominator;
   }
