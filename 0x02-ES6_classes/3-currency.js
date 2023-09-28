8a632addfc6cd1b8357535f3d7e420fa4bd2c232
chimekinglsey <onlinekingsley@gmail.com>
2023-09-28 23:44:26 +0100

    currency code and name function

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,38 @@
 export default class Currency {
-    constructor(code, name){
-        this._code = code;
-        this._name = name;
-    }
-    
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // gets the value of code
+  get code() {
+    return this._code;
+  }
+
+  /**
+     * @param {string} code
+     */
+  set code(value) {
+    if (!(typeof value === 'string')) throw new Error('Code must be a string');
+    this._code = value;
+  }
+
+  // returns the value of this._name
+  get name() {
+    return this._name;
+  }
+
+  /**
+     * @param {string} name
+     */
+  set name(value) {
+    // if(!(value instanceof String)) throw new Error('Name must be a string')
+    if (!(typeof value === 'string')) throw new Error('Name must be a string');
+    this._name = value;
+  }
+
+  // a method to display curreny code and currency name
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
