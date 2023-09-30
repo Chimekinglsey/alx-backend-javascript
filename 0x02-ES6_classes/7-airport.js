17b9e03cdb7f64e7faf38c23bd5b8105100ab611
chimekinglsey <onlinekingsley@gmail.com>
2023-09-30 14:05:56 +0100

    overriding the default toString() behaviour

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,38 @@
+export default class Airport {
+  /**
+     *
+     * @param {String} name
+     * @param {String} code
+     */
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  // Getter method to retrieve Airport.name
+  get name() {
+    return this._name;
+  }
+
+  // Setter method to encapsulate Airport.name
+  set name(value) {
+    if (typeof value === 'string') this._name = value;
+    else throw new TypeError('Name is expected to be a string');
+  }
+
+  // Getter method to retrieve Airport.code
+  get code() {
+    return this._code;
+  }
+
+  // Setter method to encapsulate Airport.code
+  set code(value) {
+    if (typeof value === 'string') this._code = value;
+    else throw new TypeError('Code is expected to be a string');
+  }
+
+  // changing the default toString()
+  toString() {
+    return `${this._code}`;
+  }
+}
