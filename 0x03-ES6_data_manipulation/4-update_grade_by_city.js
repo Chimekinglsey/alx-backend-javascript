95c57b50017e99650932a5ef2c087d8db9cd1ced
chimekinglsey <onlinekingsley@gmail.com>
2023-10-02 23:45:29 +0100

    updating student grade by city using filter and map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,28 +1,59 @@
 export default function updateStudentGradeByCity(students, city, grades) {
-  if (!Array.isArray(students) || !Array.isArray(grades)) return [];
-  const listCityStudents = [];
-  // filter students by city
-  const studentsInCity = students.filter((student) => (student.location === city ? student : null));
-  // for each student, find their grade in the grades array using the student.id
-  listCityStudents.push(
-    ...studentsInCity.map((student) => {
+  // Check if students and grades are valid arrays
+  if (!Array.isArray(students) || !Array.isArray(grades)) {
+    return [];
+  }
+
+  // Create an array to store students in the specified city with their updated grades
+  const updatedStudents = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      // Find the matching grade for the student
       const matchingGrade = grades.find((grade) => grade.studentId === student.id);
-      if (matchingGrade) {
-        student.grade = matchingGrade.grade;
-      } else {
-      // no grade associated with student, set it manually
-      // + without affecting original grade
-        student.grade = 'N/A';
-      }
-      // collate student data and append it to list
+
+      // Determine the grade value, setting to 'N/A' if not found or invalid
+      const gradeValue = matchingGrade && typeof matchingGrade.grade === 'number'
+        ? matchingGrade.grade
+        : 'N/A';
+
+      // Create a new object with the desired properties
       return {
         id: student.id,
         firstName: student.firstName,
         location: student.location,
-        grade: student.grade,
+        grade: gradeValue,
       };
-    }),
-  );
-  // return the list
-  return listCityStudents;
+    });
+
+  return updatedStudents;
 }
+// export default function updateStudentGradeByCity(students, city, grades) {
+//     if (!Array.isArray(students) || !Array.isArray(grades)) return [];
+//     const listCityStudents = [];
+//     // filter students by city
+// const studentsInCity = students.filter((student) => (student.location === city ? student : null))
+//     // for each student, find their grade in the grades array using the student.id
+
+//       [...studentsInCity].map((student) => {
+//         const matchingGrade = grades.find((grade) => grade.studentId === student.id);
+//         if (matchingGrade) {
+//           student.grade = matchingGrade.grade;
+//         } else {
+//         // no grade associated with student, set it manually
+//         // + without affecting original grade
+//           student.grade = 'N/A';
+//         }
+//         // collate student data and append it to list
+//         const studentData = {
+//           id: student.id,
+//           firstName: student.firstName,
+//           location: student.location,
+//           grade: student.grade,
+//         };
+//          listCityStudents.push(studentData)
+
+//       })
+
+//       return listCityStudents;
+//       // return the list
+//     }
