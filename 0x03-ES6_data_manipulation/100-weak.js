6a6271d1d975c5b7f85e660a2506214eabc2c050
chimekinglsey <onlinekingsley@gmail.com>
2023-10-03 20:19:39 +0100

    using WeakMap() to track API query to endpoints

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,10 +3,10 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0; // compressed if...else
-//   if (!weakMap.has(endpoint)) {
-//     weakMap.set(endpoint, 1);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, count + 1);
+  //   if (!weakMap.has(endpoint)) {
+  //     weakMap.set(endpoint, 1);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
   }
+  weakMap.set(endpoint, count + 1);
+}
