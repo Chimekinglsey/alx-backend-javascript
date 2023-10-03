b73b2693c035a213f5de0c089b65aba1f1398c06
chimekinglsey <onlinekingsley@gmail.com>
2023-10-03 20:01:09 +0100

    Count endpoint query

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,23 +1,10 @@
 // Create a WeakMap to store the number of times each endpoint is queried
 export const weakMap = new WeakMap();
 
-// Define a function to query the API and track usage for each endpoint
 export function queryAPI(endpoint) {
-  // Check if the endpoint is already in the WeakMap
-  if (!weakMap.has(endpoint)) {
-    // If not, initialize the count to 1
-    weakMap.set(endpoint, 1);
-  } else {
-    // If the endpoint is in the WeakMap, retrieve the current count
-    const count = weakMap.get(endpoint);
-
-    // Check if the count has reached the limit of 5
-    if (count >= 5) {
-      // If it has, throw an error indicating high endpoint load
-      throw new Error('Endpoint load is high');
-    }
-
-    // Increment the count for the endpoint
-    weakMap.set(endpoint, count + 1);
+  const count = weakMap.get(endpoint) || 0;
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
   }
+  weakMap.set(endpoint, count + 1);
 }
