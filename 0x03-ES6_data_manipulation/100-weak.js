a99bfeb3003eb9e99619e7fe2247e63ab384d0ae
chimekinglsey <onlinekingsley@gmail.com>
2023-10-03 19:47:47 +0100

    A queryAPI function with endpoint tracking in WeakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,23 @@
+// Create a WeakMap to store the number of times each endpoint is queried
 export const weakMap = new WeakMap();
 
-let counter = 1;
+// Define a function to query the API and track usage for each endpoint
 export function queryAPI(endpoint) {
-  function count() {
-    counter += 1;
-    return counter;
+  // Check if the endpoint is already in the WeakMap
+  if (!weakMap.has(endpoint)) {
+    // If not, initialize the count to 1
+    weakMap.set(endpoint, 1);
+  } else {
+    // If the endpoint is in the WeakMap, retrieve the current count
+    const count = weakMap.get(endpoint);
+
+    // Check if the count has reached the limit of 5
+    if (count >= 5) {
+      // If it has, throw an error indicating high endpoint load
+      throw new Error('Endpoint load is high');
+    }
+
+    // Increment the count for the endpoint
+    weakMap.set(endpoint, count + 1);
   }
-  if (counter >= 5) throw new Error('Endpoint load is high');
-  weakMap.set(endpoint, count());
 }
