2be16534816e26b90456cb1482959eb805d8de11
chimekinglsey <onlinekingsley@gmail.com>
2023-10-03 20:06:50 +0100

    A queryAPI function that counts endpoint count

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,6 +2,7 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
+  if (typeof endpoint !== 'object') throw new Error('Endpoint must be an object');
   const count = weakMap.get(endpoint) || 0;
   if (count >= 5) {
     throw new Error('Endpoint load is high');
