71661697fd74d7c86c165721c996be4abe86b977
chimekinglsey <onlinekingsley@gmail.com>
2023-10-03 02:31:23 +0100

    substring slicing

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set) || typeof startString !== 'string' || !set || !startString)
-    return ""
+  if (!(set instanceof Set) || typeof startString !== 'string' || !set || !startString) return '';
   const matches = [];
   set.forEach((key) => {
     if (typeof key === 'string' && key.startsWith(startString) && startString.length > 0) {
       // substring(remove) removes the startString and appends the rest
-      if(key.substring(startString.length) !== key)
-        matches.push(key.substring(startString.length));
+      matches.push(key.substring(startString.length));
     }
   });
 
