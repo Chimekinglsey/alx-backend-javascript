1dc0a189190509d55212aaa69cef63019c526e37
chimekinglsey <onlinekingsley@gmail.com>
2023-10-03 02:45:28 +0100

    9. Map data structure

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
   const matches = [];
   set.forEach((key) => {
     if (typeof key === 'string' && key.startsWith(startString) && startString.length > 0) {
-      // substring(remove) removes the startString and appends the rest
+      // substring() removes the startString and appends the rest
       matches.push(key.substring(startString.length));
     }
   });
