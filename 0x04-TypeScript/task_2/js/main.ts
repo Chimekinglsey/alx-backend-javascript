e15ca86307628e2f18af77d745a162413bf35848
chimekinglsey <onlinekingsley@gmail.com>
2023-10-05 16:15:14 +0100

    using Union to create type

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -32,7 +32,37 @@ class Teacher implements TeacherInterface {
   getCoffeeBreak = () : string => "Cannot have a break"
   workTeacherTasks = () :string => "Getting to work"
 }
-const createEmployee = (salary: string | number) => typeof salary === 'number' && salary < 500? console.log("Teacher"): console.log("Director");
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+const createEmployee = (salary: string | number) => typeof salary === 'number' && salary < 500? "Teacher" : "Director";
+
+// console.log(createEmployee(200));
+// console.log(createEmployee(1000));
+// console.log(createEmployee('$500'));
+
+//isDirector Function
+function isDirector(employee: any): employee is Director{
+  return employee instanceof Director;
+}
+function executeWork(employee: any){
+  if(employee === 'Director'){
+    const value = new Director();
+    return value.workDirectorTasks()
+  }
+  else{
+    const value = new Teacher();
+    return value.workTeacherTasks()
+  }
+}
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+//String literal type
+type Subjects = "Math" | "History";
+
+const teachClass = (todayClass: Subjects) => {
+  if (todayClass === 'History')
+  return `teaching ${todayClass}`
+  if (todayClass === 'Math')
+  return`teaching ${todayClass}`
+}
+console.log(teachClass('Math'));
+ 
