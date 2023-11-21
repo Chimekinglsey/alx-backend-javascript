7aad16e5bc6be92de2e85a82cd4de1c26eaaf331
chimekinglsey <onlinekingsley@gmail.com>
2023-11-21 17:08:02 +0100

    Using process stdin/stdout

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-process.stdin.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) console.log(`Your name is: ${name}`);
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
-process.on('close', () => {
-  console.log('his important software is now closing');
+process.stdin.on('close', () => {
+  console.log('This important software is now closing');
 });
