1572e4a125f30db7d8bda9402e5716f7a9c1efe9
chimekinglsey <onlinekingsley@gmail.com>
2023-11-21 18:12:58 +0100

    Sync Read from CSV

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,55 +1,39 @@
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (dbPath) => {
   try {
-    // Read the database file synchronously
-    const data = fs.readFileSync(path, 'utf8');
+    const content = fs.readFileSync(dbPath, 'utf-8');
+    const lines = content.split('\n');
 
-    // Split the file into lines and filter out empty lines
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const validLines = lines.filter(line => line.trim() !== '');
+    const totalStudents = validLines.length - 1;
 
-    // Calculate the total number of students
-    const totalStudents = lines.length - 1; // Subtract 1 to exclude the header line
+    const fieldCounts = {
+      'SWE': [],
+      'CS': []
+    };
 
-    // Log the total number of students
-    console.log(`Number of students: ${totalStudents}`);
+    if (totalStudents > 0) {
+      for (const line of validLines) {
+        const fields = line.split(',');
+        const field = fields[3] ? fields[3].trim() : '';
 
-    // Extract and count students in each field
-    const fieldCounts = {};
+        if (field === 'SWE') {
+          fieldCounts['SWE'].push(fields[0]);
+        } else if (field === 'CS') {
+          fieldCounts['CS'].push(fields[0]);
+        }
+      }
 
-    for (let i = 1; i < lines.length; i++) { // Start from index 1 to skip the header line
-      const fields = lines[i].split(',');
-      const field = fields[3].trim(); // Assuming the field is in the fourth column
-
-      if (fieldCounts[field]) {
-        fieldCounts[field]++;
-      } else {
-        fieldCounts[field] = 1;
+      console.log(`Number of students: ${totalStudents}`);
+      for (const field in fieldCounts) {
+        const count = fieldCounts[field].length;
+        console.log(`Number of students in ${field}: ${count}. List: ${fieldCounts[field].join(', ')}`);
       }
     }
-
-    // Log the number of students in each field
-    for (const field in fieldCounts) {
-      console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${getFirstNamesList(lines, field)}`);
-    }
   } catch (error) {
-    // Log an error message if the database is not available
-    console.error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
-}
-function getFirstNamesList(lines, field) {
-  const firstNamesList = [];
-
-  for (let i = 1; i < lines.length; i++) { // Start from index 1 to skip the header line
-    const fields = lines[i].split(',');
-    const currentField = fields[3].trim(); // Assuming the field is in the fourth column
-
-    if (currentField === field) {
-      firstNamesList.push(fields[0].trim()); // Assuming the first name is in the first column
-    }
-  }
-
-  return firstNamesList.join(', ');
-}
+};
 
 module.exports = countStudents;
