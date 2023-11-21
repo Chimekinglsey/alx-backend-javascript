b644d8d3ce9c8b51321e945d9b610810a95b35d0
chimekinglsey <onlinekingsley@gmail.com>
2023-11-21 13:47:27 +0100

    Using process.stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,30 +1,10 @@
-const { spawn } = require('child_process');
-
-const ls = spawn('cat');
-
 console.log('Welcome to Holberton School, what is your name?');
-// isTTY helps to ensure that the data is coming from keyboard directly
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-  });
-// This will run when data is entered as prompt, like cat or echo
-} else {
-  let pipedInput = '';
-  process.stdin.on('data', (input) => {
-    pipedInput += input;
-  });
-
-  process.stdin.on('end', () => {
-    const name = pipedInput.trim();
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    process.exit(0);
-  });
-}
-
-ls.on('exit', () => {
+process.stdin.once('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+  process.exit(0);
+});
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
   process.exit(0);
 });
