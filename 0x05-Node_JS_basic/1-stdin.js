77a9a83cd3c3245bdbdedc991ff4825fbbf32ec2
chimekinglsey <onlinekingsley@gmail.com>
2023-11-21 13:49:18 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,6 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.once('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.exit(0);
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
