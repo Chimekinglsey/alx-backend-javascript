18ce74fe4f2f27eb9f1441e60ec52daeeff48bab
chimekinglsey <onlinekingsley@gmail.com>
2023-11-21 21:31:45 +0100

    Simple Express Server

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const port = 1245;
+const app = express();
+app.get('/', (req, resp) => {
+  resp.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening as port  ${port}`);
+});
