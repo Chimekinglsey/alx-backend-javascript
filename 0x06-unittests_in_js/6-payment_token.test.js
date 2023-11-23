c82d7982f77efe80b6e82cfc659d16b892570798
chimekinglsey <onlinekingsley@gmail.com>
2023-11-24 00:34:50 +0100

    Testing promises with done callback

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,13 +5,16 @@ chai.use(require("chai-as-promised"));
 const getPaymentTokenFromAPI = require('./6-payment_token')
 
 describe('getPaymentTokenFromAPI', () => {
-    it('Should return payload when `success` is true', () => {
+    it('Should return payload when `success` is true', (done) => {
         expect(getPaymentTokenFromAPI(true)).to.eventually.equal({data: 'Successful response from the API' })
+        done()
     })
-    it('Should not return payload when `success` is false', () => {
+    it('Should not return payload when `success` is false', (done) => {
         expect(getPaymentTokenFromAPI(false)).to.eventually.be.false
+        done()
     })
-    it('Should not return false when success is not true', () => {
+    it('Should not return false when success is not true', (done) => {
         expect(getPaymentTokenFromAPI(false)).to.eventually.be.false
+        done()
     })
 })
