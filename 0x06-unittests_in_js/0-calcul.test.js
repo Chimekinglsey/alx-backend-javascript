194b7d4c2e557e8e7119a2dbca803b01e04c9284
chimekinglsey <onlinekingsley@gmail.com>
2023-11-23 20:52:47 +0100

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,18 +1,29 @@
 const calculateNumber = require('./0-calcul')
-const assert = require('chai').assert
+const assert = require('assert')
 
 describe('Calc', function() {
     describe('calculateNumber', function(){
         it('Should return the rounded sum of two numbers', function (){
-            assert.equal(calculateNumber(20.5, 10.3), 31, 'round(20.5) and round(10.3) is 31');
-            assert.equal(calculateNumber(20.4, 10.3), 30, 'round(20.5) and round(10.3) is 31')
-            assert.equal(calculateNumber(20.0, 10), 30, 'round(20.5) and round(10.3) is 31')
-            assert.isNumber(calculateNumber(10, 0))
+            assert.equal(calculateNumber(20.5, 10.3), 31);
+            assert.equal(calculateNumber(12.2, 3.7), 16)
+            assert.equal(calculateNumber(0.6, 3), 4)
 
         });
         it('Should return the rounded sum of two numbers', function (){
-            assert.equal(calculateNumber(20.0, 10), 30, 'round(20.5) and round(10.3) is 31')
-            assert.isNumber(calculateNumber(10, 0))
+            assert.equal(calculateNumber(-0.6, 3), 2)
+            assert.equal(calculateNumber(20.0, -10), 10)
+            assert.equal(calculateNumber(-1.6, 3.7), 2)
+
+        });
+        it('Should return the rounded sum of two numbers', function (){
+            assert.equal(calculateNumber(1, 3.7), 5)
+            assert.equal(calculateNumber(1.1, 2.95), 4)
+
+        });
+        it('Should return the rounded sum of two numbers', function (){
+            assert.equal(calculateNumber(0, -0.7), -1)
+            assert.equal(calculateNumber(20.0, 10), 30)
+            assert.equal(calculateNumber(1.5, 3.7), 6)
 
         });
     })
