ad9b4cc17132018aef2da5c91d8ef4bdf5c8f166
chimekinglsey <onlinekingsley@gmail.com>
2023-11-23 21:44:49 +0100

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,30 +1,26 @@
-const calculateNumber = require('./0-calcul')
-const assert = require('assert')
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe('Calc', function() {
-    describe('calculateNumber', function(){
-        it('Should return the rounded sum of two numbers', function (){
-            assert.equal(calculateNumber(20.5, 10.3), 31);
-            assert.equal(calculateNumber(12.2, 3.7), 16)
-            assert.equal(calculateNumber(0.6, 3), 4)
-
-        });
-        it('Should return the rounded sum of two numbers', function (){
-            assert.equal(calculateNumber(-0.6, 3), 2)
-            assert.equal(calculateNumber(20.0, -10), 10)
-            assert.equal(calculateNumber(-1.6, 3.7), 2)
-
-        });
-        it('Should return the rounded sum of two numbers', function (){
-            assert.equal(calculateNumber(1, 3.7), 5)
-            assert.equal(calculateNumber(1.1, 2.95), 4)
-
-        });
-        it('Should return the rounded sum of two numbers', function (){
-            assert.equal(calculateNumber(0, -0.7), -1)
-            assert.equal(calculateNumber(20.0, 10), 30)
-            assert.equal(calculateNumber(1.5, 3.7), 6)
-
-        });
-    })
-})
+describe('calc', () => {
+  describe('calculateNumber', () => {
+    it('should return the rounded sum of two numbers', () => {
+      assert.equal(calculateNumber(20.5, 10.3), 31);
+      assert.equal(calculateNumber(12.2, 3.7), 16);
+      assert.equal(calculateNumber(0.6, 3), 4);
+    });
+    it('should return the rounded sum of two numbers', () => {
+      assert.equal(calculateNumber(-0.6, 3), 2);
+      assert.equal(calculateNumber(20.0, -10), 10);
+      assert.equal(calculateNumber(-1.6, 3.7), 2);
+    });
+    it('should return the rounded sum of two numbers', () => {
+      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(1.1, 2.95), 4);
+    });
+    it('should return the rounded sum of two numbers', () => {
+      assert.equal(calculateNumber(0, -0.7), -1);
+      assert.equal(calculateNumber(20.0, 10), 30);
+      assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+  });
+});
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,28 +1,28 @@
-const calculateNumber = require('./1-calcul')
-const expect = require('chai').expect
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
 
-describe('CALCULATE NUMBERS', function(){
-    describe('SUM', function (){
-        it('Should return the rounded "SUM" of two numbers', function () {
-            expect(calculateNumber('SUM', 20.5, 10.3), 'Rounded sum of two positive ints').to.equal(31);
-            expect(calculateNumber("SUM", 12.2, 3.7), 'Rounded sum of two positive ints').to.equal(16)
-            expect(calculateNumber("SUM", 0.6, 3), 'Rounded sum of two positive ints').to.equal(4)
-        });
-    })
+describe('cALCULATE NUMBERS', () => {
+  describe('sUM', () => {
+    it('should return the rounded "SUM" of two numbers', () => {
+      expect(calculateNumber('SUM', 20.5, 10.3), 'Rounded sum of two positive ints').to.equal(31);
+      expect(calculateNumber('SUM', 12.2, 3.7), 'Rounded sum of two positive ints').to.equal(16);
+      expect(calculateNumber('SUM', 0.6, 3), 'Rounded sum of two positive ints').to.equal(4);
+    });
+  });
 
-    describe('SUBTRACT', function (){
-        it('Should return the rounded "DIFFERENCE" of two numbers', function (){
-            expect(calculateNumber("SUBTRACT", -0.6, 3), 'Rounded diff of +ve and -ve ints').to.equal(-4)
-            expect(calculateNumber("SUBTRACT", 20.0, -10), 'Rounded diff of +ve and -ve ints').to.equal(30)
-            expect(calculateNumber("SUBTRACT", -1.6, 3.7), 'Rounded diff of +ve and -ve ints').to.equal(-6)
-        });
-    })
+  describe('sUBTRACT', () => {
+    it('should return the rounded "DIFFERENCE" of two numbers', () => {
+      expect(calculateNumber('SUBTRACT', -0.6, 3), 'Rounded diff of +ve and -ve ints').to.equal(-4);
+      expect(calculateNumber('SUBTRACT', 20.0, -10), 'Rounded diff of +ve and -ve ints').to.equal(30);
+      expect(calculateNumber('SUBTRACT', -1.6, 3.7), 'Rounded diff of +ve and -ve ints').to.equal(-6);
+    });
+  });
 
-    describe('DIVIDE', function (){ 
-        it('Should return the rounded "DIVISION" of two numbers', function (){
-            expect(calculateNumber("DIVIDE", 1, 3.7), 'Rounded division of +ve integers').to.equal(0.25)
-            expect(calculateNumber("DIVIDE", 10, 2), 'Rounded division of +ve integers').to.equal(5)
-            expect(calculateNumber("DIVIDE", 10, 0), 'Rounded division by zero').to.be.a('string')
-        });
-    })
-})
+  describe('dIVIDE', () => {
+    it('should return the rounded "DIVISION" of two numbers', () => {
+      expect(calculateNumber('DIVIDE', 1, 3.7), 'Rounded division of +ve integers').to.equal(0.25);
+      expect(calculateNumber('DIVIDE', 10, 2), 'Rounded division of +ve integers').to.equal(5);
+      expect(calculateNumber('DIVIDE', 10, 0), 'Rounded division by zero').to.be.a('string');
+    });
+  });
+});
