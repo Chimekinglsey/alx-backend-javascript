1a1c3c3a1d098ad0e54fa9d0f4c31c5c9e6fc462
chimekinglsey <onlinekingsley@gmail.com>
2023-11-24 03:09:23 +0100

    Testing edge cases

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,18 +1,20 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
-port = 7865;
+const PORT = 7865;
 
-app.get('/', (req, res)=>{
-    res.send('Welcome to the payment system')
-})
-app.get('/cart/:id(\\d+)', (req, res) =>{
-    const id = req.params.id;
-    res.send(`Payment methods for cart ${id}`)
-})
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
 
-app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`)
-})
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
 
 module.exports = app;
