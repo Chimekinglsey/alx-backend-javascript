e519690469532271d37035ae1f5dd864fe967345
chimekinglsey <onlinekingsley@gmail.com>
2023-11-24 03:42:53 +0100

    Regex Integration test

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,47 +2,35 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('API integration test', () => {
-  const url = 'http://localhost:7865/cart';
-//   it('should return 200 status code', (done) => {
-//     request.get(`${url}/12`, (_err, res, _body) => {
-//       expect(res.statusCode).to.be.equal(200);
-//       done();
-//     });
-//   })
+  const API_URL = 'http://localhost:7865';
 
-//   it('Should print response body', (done) => {
-//     request.get(`${url}/12`, (_err, _res, body) => {
-//         expect(body).to.be.equal('Payment methods for cart 12');
-//         done();
-//     });
-//   })
-
-//   it('Should print 404', (done) => {
-//     request.get(`${url}/NaN`, (_err, res, _body) => {
-//         expect(res.statusCode).to.be.equal(404);
-//         done();
-//     });
-//   })
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
 
   it('GET /cart/:id returns correct response for valid :id', (done) => {
-    request.get(`${url}/47`, (_err, res, body) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart 47');
       done();
     });
-  })
+  });
 
   it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
-    request.get(`${url}/-47`, (_err, res, _body) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
-  })
+  });
 
   it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
-    request.get(`${url}/d200-44a5-9de6`, (_err, res, _body) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
-  })
+  });
 });
