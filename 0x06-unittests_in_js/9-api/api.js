71e746bc4ea576491b8dc81234710f495a43ded9
chimekinglsey <onlinekingsley@gmail.com>
2023-11-24 02:53:58 +0100

    Regex path

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,7 +6,7 @@ port = 7865;
 app.get('/', (req, res)=>{
     res.send('Welcome to the payment system')
 })
-app.get('/cart/:id', (req, res) =>{
+app.get('/cart/:id(\\d+)', (req, res) =>{
     const id = req.params.id;
     res.send(`Payment methods for cart ${id}`)
 })
